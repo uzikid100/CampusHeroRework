ef98f8dc0a72a63ff0775dcdb853c32f85dd5b6e
uzikid100 <uzeziogodo@gmail.com>
2017-10-04 15:07:46 -0700

    fixed double click bug. Added Superman Logo for login in activity.

--- app/src/main/java/com/example/uzezi/campushero3/MainActivity.java
+++ app/src/main/java/com/example/uzezi/campushero3/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.uzezi.campushero3;
 
-import android.icu.text.DisplayContext;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -11,20 +9,23 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     private SectionsPageAdapter mSectionsPageAdapter;
     private ViewPager mViewPager;
 
     private MapFragment mMapFragment;
+    private ProfileFragment mProfileFragment;
+    private POIFragment mPoiFragment;
 
     private LinearLayout mFabMapNormalLayout;
     private LinearLayout mFabMapHybridLayout;
@@ -32,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout mFabMapSatelliteLayout;
 
     private boolean fabExpanded = false;
-    private FloatingActionButton fabSettings;
 
+    private FloatingActionButton mFabLocation;
+    private FloatingActionButton mFabMapType;
     private FloatingActionButton fabNormal;
     private FloatingActionButton fabHybrid;
     private FloatingActionButton fabTerrain;
@@ -43,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout.Tab mProfileTab;
     private TabLayout.Tab mPoiTab;
 
-    private FloatingActionButton mFabLocation;
-    private FloatingActionButton mFabMapType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager);
 
-
         //Sets the tab layout to the maps Fragment
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
@@ -67,40 +67,27 @@ public class MainActivity extends AppCompatActivity {
         mPoiTab = tabLayout.getTabAt(2);
         mMapTab.select();
 
-
         //Finds the layouts for fab action menu
         mFabMapNormalLayout = (LinearLayout) findViewById(R.id.layoutFabNormal);
         mFabMapHybridLayout= (LinearLayout) findViewById(R.id.layoutFabHybrid);
         mFabMapTerrainLayout= (LinearLayout) findViewById(R.id.layoutFabTerrain);
         mFabMapSatelliteLayout = (LinearLayout) findViewById(R.id.layoutFabSatellite);
 
-
         //Gets references to the Fabs in Menu
+        mFabMapType = (FloatingActionButton) findViewById(R.id.fabMapType);
+        mFabLocation = (FloatingActionButton) findViewById(R.id.fabLocation);
         fabNormal = (FloatingActionButton) findViewById(R.id.fabMapNormal);
         fabHybrid = (FloatingActionButton) findViewById(R.id.fabMapHybrid);
         fabTerrain = (FloatingActionButton) findViewById(R.id.fabMapTerrain);
         fabSatellite= (FloatingActionButton) findViewById(R.id.fabMapSatellite);
 
-        mFabLocation = (FloatingActionButton) findViewById(R.id.fab_current_location);
-        mFabLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mMapFragment.goToCurrentLocation();
-            }
-        });
+        mFabMapType.setOnClickListener(this);
+        mFabLocation.setOnClickListener(this);
+        fabNormal.setOnClickListener(this);
+        fabHybrid.setOnClickListener(this);
+        fabTerrain.setOnClickListener(this);
+        fabSatellite.setOnClickListener(this);
 
-
-        mFabMapType = (FloatingActionButton) findViewById(R.id.fab);
-        mFabMapType.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (fabExpanded == true){
-                    closeSubMenusFab();
-                } else {
-                    openSubMenusFab();
-                }
-                }
-            });
         closeSubMenusFab();
 
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -124,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    }////////End of OnCreate////////
 
     private void disableFabs(){
         mFabLocation.setVisibility(View.INVISIBLE);
@@ -146,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
         fabExpanded = false;
     }
 
-    //Opens FAB submenus
     private void openSubMenusFab(){
         mFabMapNormalLayout.setVisibility(View.VISIBLE);
         mFabMapHybridLayout.setVisibility(View.VISIBLE);
@@ -160,10 +146,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         mMapFragment = new MapFragment();
+        mProfileFragment = new ProfileFragment();
+        mPoiFragment = new POIFragment();
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ProfileFragment(), "Profile");
+        adapter.addFragment(mProfileFragment, "Profile");
         adapter.addFragment(mMapFragment, "Campus Map");
-        adapter.addFragment(new ProfileFragment(), "Points Of Interest");
+        adapter.addFragment(mPoiFragment, "Points Of Interest");
         viewPager.setAdapter(adapter);
     }
 
@@ -190,24 +178,32 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onFabMenuItemSelected(View view) {
-        try{
-            final FloatingActionButton fab = (FloatingActionButton) view;
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int id = fab.getId();
-                    if(id == fabNormal.getId())
-                        mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_NORMAL);
-                    if(id == fabHybrid.getId())
-                        mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_HYBRID);
-                    if(id == fabTerrain.getId())
-                        mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                    if(id == fabSatellite.getId())
-                        mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                    if(fabExpanded == true) closeSubMenusFab();
-                }
-            });
-        }catch (ClassCastException e){}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fabMapType:
+                if(fabExpanded) {closeSubMenusFab();}
+                  else {openSubMenusFab();}
+                break;
+            case R.id.fabLocation:
+                mMapFragment.goToCurrentLocation();
+            case R.id.fabMapNormal:
+                mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_NORMAL);
+                if(fabExpanded) {closeSubMenusFab();}
+                break;
+            case R.id.fabMapHybrid:
+                mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_HYBRID);
+                if(fabExpanded) {closeSubMenusFab();}
+                break;
+            case R.id.fabMapTerrain:
+                mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                if(fabExpanded) {closeSubMenusFab();}
+                break;
+            case R.id.fabMapSatellite:
+                mMapFragment.ChangeMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                if(fabExpanded) {closeSubMenusFab();}
+                break;
+        }
     }
+
 }
